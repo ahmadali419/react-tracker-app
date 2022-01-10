c05be04bd8ad762b5d15dd98f3df31c05224dab0
aiman-arainx <84847654+aiman-arainx@users.noreply.github.com>
2022-01-10 18:44:21 +0500

    task tracker

--- src/App.js
+++ src/App.js
@@ -1,48 +1,86 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from "react";
+import About from "./components/About";
+import Footer from "./components/Footer";
 function App() {
-  const [tasks,setTasks] = useState([
-    {
-        id:1,
-        text: 'Food shopping',
-        day:  '5th jan 2022',
-        reminder: true
-    },
-    {
-        id:2,
-        text: 'Rainy Day',
-        day:  '5th jan 2022',
-        reminder: true
-    },
-    {
-        id:3,
-        text: 'Food shopping',
-        day:  '5th jan 2022',
-        reminder: false
-    }
-]);
-const deleteTask = (id) =>{
-      setTasks(tasks.filter((task) => task.id !== id))
-}
-const toggleReminder = (id) =>{
-  setTasks(tasks.map((task) => task.id == id ? {...task, reminder:!task.reminder} : task))
-}
-const addTask = (task) =>{
-  console.log(task);
-  const id = Math.floor(Math.random() * 10000) +  1;
-  const newTask = {id , ...task};
-  setTasks([...tasks, newTask]);
 
-} 
-const [showTask , setShowTask] = useState(false);
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
+  fetchTasks()
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'delete',
+    })
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = async (id) => {
+    const toggleToTask = await fetchTask(id)
+    const updTask = { ...toggleToTask, reminder: !toggleToTask.reminder }
+    const res = await fetch(
+      `http://localhost:5000/tasks/${id}`, {
+      method: 'Put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updTask)
+    }
+    );
+    const data = await res.json();
+    setTasks(tasks.map((task) => task.id == id ? { ...task, reminder: data.reminder } : task))
+  }
+
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(task)
+    });
+    const data = await res.json();
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    setTasks([...tasks, data]);
+
+  }
+
+  const [showAddTask, setShowAddTask] = useState(false);
+
   return (
-    <div className="container">
-       <Header onAdd = {()=> setShowTask(!showTask)} />
-       {showAddTask && <AddTask onAdd={addTask}  />}
-       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task'}
-    </div>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Route path='/' exact render={(props) => {
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task'}
+          </>
+        }} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+
+export const About = () => {
+    return (
+        <div>
+            <p>16.0.1</p>
+            <a href='/'>Go back</a>
+        </div>
+    )
+}
+export default About;
+
